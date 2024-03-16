b4e6f85ef90a4712b7d951d0dbc7e43e9b2a18a1
Mantavya <mantavyamahajan@gmail.com>
2024-03-16 19:59:22 -0400

    Co-authored-by: Aayush9930 <Aayush9930@users.noreply.github.com> Co-authored-by: Suryanshagrawal19 <Suryanshagrawal19@users.noreply.github.com>

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>/", views.entry, name="entry"),
-    path("search/", views.search, name="search"),
-    path("new/", views.new, name="new"),
-    path("wiki/<str:title>/edit", views.edit, name="edit"),
-    path("random/", views.random, name="random")
+    path("login/", views.login, name="login"),
+    path('signup/', views.signup, name='signup'),
 ]
